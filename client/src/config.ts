22c76c32e330a7308c42eb60527754a49c825536
johan <stjernholm@msn.com>
2020-01-30 23:08:45 +0800

    Feature: add secondary index to db

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'dsnpy420kd'
+const apiId = 'ezi297m0h8'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
